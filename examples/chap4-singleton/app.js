37c81e3a40868cd3c9f7683bbcd17f2f55335730
zsqzsq1993 <zsqzsq1993@yeah.net>
2020-07-11 15:47:30 +0800

    init test code for chap4 singleton

---
+++ examples/chap4-singleton/app.js
@@ -0,0 +1,120 @@
+function basicSingletonES5() {
+    function Singleton(name) {
+        this.name = name
+    }
+
+    Singleton.prototype.sayName = function () {
+        console.log(this.name)
+    }
+
+    Singleton.getInstance = function (name) {
+        if (!this.instance) {
+            this.instance = new Singleton(name)
+        }
+        return this.instance
+    }
+
+    testBasicSingleton(Singleton)
+}
+
+function basicSingletonES6() {
+    class Singleton {
+        constructor(name) {
+            this.name = name
+        }
+
+        sayName() {
+            console.log(this.name)
+        }
+
+        static getInstance(name) {
+            if (!this.instance) {
+                this.instance = new Singleton(name)
+            }
+            return this.instance
+        }
+    }
+
+    testBasicSingleton(Singleton)
+}
+
+function basicSingletonClosure() {
+    const Singleton = function (name) {
+        this.name = name
+    }
+    Singleton.prototype.sayName = function () {
+        console.log(this.name)
+    }
+    Singleton.getInstance = (function () {
+        let instance = null
+        return function (name) {
+            if (!instance) {
+                instance = new Singleton(name)
+            }
+            return instance
+        }
+    })()
+    testBasicSingleton(Singleton)
+}
+
+function testBasicSingleton(Singleton) {
+    const obj1 = Singleton.getInstance('Jack')
+    const obj2 = Singleton.getInstance('Tom')
+    console.log(obj1 === obj2) // true
+    obj1.sayName() // Jack
+    obj2.sayName() // Jack
+    console.log(obj1.instance) // undefined
+    console.log(obj2.instance) // undefined
+}
+
+function moreGeneralSingleton() {
+    const CreateElement = (function () {
+        let instance = null
+        const RetConstructor = function (html) {
+            if (!instance) {
+                this.html = html
+                this.init()
+                instance = this
+            }
+            return instance
+        }
+        RetConstructor.prototype.init = function () {
+            const div = document.createElement('div')
+            div.innerText = this.html
+            document.body.appendChild(div)
+        }
+        return RetConstructor
+    })()
+
+    const div1 = new CreateElement('div1')
+    const div2 = new CreateElement('div2')
+    console.log(div1 === div2)
+    console.log(div1.html)
+    console.log(div2.html)
+}
+
+function myThink() {
+    class CreateElement{
+        constructor(html) {
+            if (!CreateElement.instance) {
+                this.html = html
+                this.init()
+                CreateElement.instance = this
+            }
+            return CreateElement.instance
+        }
+        init() {
+            const div = document.createElement('div')
+            div.innerText = this.html
+            document.body.appendChild(div)
+        }
+    }
+
+    const div1 = new CreateElement('div1')
+    const div2 = new CreateElement('div2')
+    console.log(div1 === div2)
+    console.log(div1.html)
+    console.log(div2.html)
+}
+
+myThink()
