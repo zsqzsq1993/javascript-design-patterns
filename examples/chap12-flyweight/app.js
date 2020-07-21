0cac2559d8be61958b53271c0c54b0e6969ccb53
zsqzsq1993 <zsqzsq1993@yeah.net>
2020-07-21 23:17:25 +0800

    chap12-flyweight

---
+++ examples/chap12-flyweight/app.js
@@ -0,0 +1,162 @@
+function normalUpload() {
+    const startUpLoad = (function () {
+        let uid = 0
+        return function (fileType, files) {
+            class UpLoad {
+                constructor(fileType, file, id) {
+                    this.id = id
+                    this.fileType = fileType
+                    this.fileSize = file.size
+                    this.fileName = file.name
+                    this.dom = null
+                    this._init()
+                }
+
+                _init() {
+                    this.dom = document.createElement('div')
+                    this.dom.innerHTML =
+                        `<span>id: ${this.id}</span> ` +
+                        `<span>fileType: ${this.fileType}</span>` +
+                        `<span>filenName: ${this.fileName} </span>` +
+                        `<span>fileSize: ${this.fileSize} </span>` +
+                        `<button>delete me</button>`
+                    const btn = this.dom.querySelector('button')
+                    btn.onclick = () => {
+                        this._delFile()
+                    }
+                    document.body.appendChild(this.dom)
+                }
+
+                _delFile() {
+                    if (this.fileSize < 3000) {
+                        this.dom.parentNode.removeChild(this.dom)
+                    } else {
+                        if (confirm('Are you sure to delete?')) {
+                            this.dom.parentNode.removeChild(this.dom)
+                        }
+                    }
+                }
+            }
+
+            files.forEach(file => {
+                const obj = new UpLoad(fileType, file, uid++)
+            })
+        }
+    })()
+
+    startUpLoad('flash', [
+        {
+            name: '1.flash',
+            size: 4000
+        },
+        {
+            name: '2.flash',
+            size: 1500
+        }
+    ])
+
+    startUpLoad('plugin', [
+        {
+            name: '1.plg',
+            size: 4000
+        },
+        {
+            name: '2.plg',
+            size: 1500
+        }
+    ])
+}
+
+function flyweightUpload() {
+    let uid = 0
+
+    class Upload {
+        constructor(fileType) {
+            this.fileType = fileType
+        }
+
+        _delFile() {
+            if (this.fileSize < 3000) {
+                this.dom.parentNode.removeChild(this.dom)
+            } else {
+                if (confirm('Are you sure to delete?')) {
+                    this.dom.parentNode.removeChild(this.dom)
+                }
+            }
+        }
+    }
+
+    const createFlyWeightObj = (function () {
+        const cache = {}
+        return function (fileType) {
+            if (cache[fileType]) {
+                return cache[fileType]
+            } else {
+                return cache[fileType] = new Upload(fileType)
+            }
+        }
+    })()
+
+    const uploadManager = (function () {
+        const cache = {}
+        return {
+            add(fileType, file, id) {
+                const dom = document.createElement('div')
+                dom.innerHTML =
+                    `<span>id: ${id}</span> ` +
+                    `<span>fileType: ${fileType}</span>` +
+                    `<span>filenName: ${file.fileName} </span>` +
+                    `<span>fileSize: ${file.fileSize} </span>` +
+                    `<button>delete me</button>`
+                dom.querySelector('button').onclick = () => {
+                    const flyweightObj = createFlyWeightObj(fileType)
+                    this.setExternal(id, flyweightObj)
+                    flyweightObj._delFile()
+                }
+                document.body.appendChild(dom)
+                cache[id] = {
+                    dom,
+                    fileType,
+                    fileName: file.fileName,
+                    fileSize: file.fileSize
+                }
+            },
+
+            setExternal(id, obj) {
+                for (let i in cache[id]) {
+                    obj[i] = cache[id][i]
+                }
+            }
+        }
+    })()
+
+    function startUp(type, files) {
+        files.forEach(file => {
+            uploadManager.add(type, file, uid++)
+        })
+    }
+
+    startUp('flash', [
+        {
+            name: '1.flash',
+            size: 4000
+        },
+        {
+            name: '2.flash',
+            size: 1500
+        }
+    ])
+
+    startUp('plugin', [
+        {
+            name: '1.plg',
+            size: 4000
+        },
+        {
+            name: '2.plg',
+            size: 1500
+        }
+    ])
+}
+
+flyweightUpload()
